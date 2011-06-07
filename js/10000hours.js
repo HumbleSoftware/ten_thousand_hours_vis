fcbae76f861d17a1d2dd1b70d1075426e3f09e07
Carl Sutherland <carl@humblesoftware.com>
2011-06-07 09:19:58 -0400

    Updated date formatter and added flag for current progress.

--- js/10000hours.js
+++ js/10000hours.js
@@ -31,9 +31,17 @@ Event.observe(document, 'dom:loaded', function() {
     
     HumbleFinance.xTickFormatter = function (n) { 
         if (n == 0) return '';
-        return dates[n];
+        var tickDate = (new Date(Date.parse(dates[0]) + n * 1000 * 60 * 60 * 24));
+        return tickDate.getMonth()+'/'+tickDate.getDate()+'/'+tickDate.getFullYear();
     };
     
     HumbleFinance.init('humblefinance', totals, practices, totals);
-//    HumbleFinance.setFlags(flagData);
+
+    var message, flagData;
+
+    message = '<div>'+Math.round(totals[totals.length-1][1])+' Total Hours, ';
+    message += dates[totals.length-1]+'</div>';
+    flagData = [[totals.length-1, message]];
+
+    HumbleFinance.setFlags(flagData);
 });
